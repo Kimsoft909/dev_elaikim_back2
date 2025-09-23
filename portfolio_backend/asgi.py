0ac9fafacc6ac14fa9e2bb15455c42d7ea935efb
Kimsoft909 <eliakimghati@gmail.com>
2025-09-23 11:45:39 +0300

    Initial commit for Django Backend Assets

--- portfolio_backend/asgi.py
+++ portfolio_backend/asgi.py
@@ -6,6 +6,6 @@ Configured to use Daphne as the default ASGI server for production deployment.
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings.production')
 
 application = get_asgi_application()
